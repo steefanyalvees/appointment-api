de01a67911051a3c0055c4c46c2788e7c9f07ae4
steefanyalvees <stefanyanjoos@hotmail.com>
2026-08-24 21:44:02 +0100

    first commit

---
+++ src/test/java/com/stefany/appointment_api/AppointmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefany.appointment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
